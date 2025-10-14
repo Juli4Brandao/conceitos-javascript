8e5da9f924845e4a097b816ffe593d34092f3915
Julia <juliaandreza97@gmail.com>
2025-10-13 21:00:54 -0300

    Primeiro Push

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log("Olá!") 
+
+let nomeCompleto = "Júlia"
+let idade = 20
+let temHabilitacao = true
+let peso = null
+
+const altura = 1.68
+
+console.log(idade)
+
+let carroCor = "Vermelho"
+let carroAno = 2018
+let carroModelo = "Onix"
+let carroMotor= 1.6
+
+let carro = {
+    cor : "vermelho" , 
+    ano: 2018,
+    modelo: "Onix",
+    motor: 1.6
+}
+console.log(carro)
